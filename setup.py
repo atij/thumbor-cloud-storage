50d47443252385f258a43d65eb2049dd0e440a32
Andrei Tij <andrei.tij@adoreme.com>
2020-07-08 09:30:01 +0300

    add multiple bucket support

--- setup.py
+++ setup.py
@@ -2,18 +2,18 @@ from setuptools import setup
 from setuptools import find_packages
 
 REQUIREMENTS = [
-  "thumbor==5.0.3",
-  "gcloud==0.7.1",
-  "protorpc==0.10.0",
+  "thumbor",
+  "gcloud",
+  "protorpc",
 ]
 
 setup(
   name="thumbor_cloud_storage",
   version="2.0.1",
-  author="Pedro Gimenez",
+  author="Andrei Tij",
   author_email="me@pedro.bz",
   description="Thumbor's Google Cloud Storage loader and result storage",
-  url="https://github.com/pedrogimenez/thumbor-cloud-storage",
+  url="https://github.com/atij/thumbor-cloud-storage",
   license="MIT",
   include_package_data=True,
   packages=find_packages(),
--- thumbor_cloud_storage/loaders/cloud_storage_loader.py
+++ thumbor_cloud_storage/loaders/cloud_storage_loader.py
@@ -1,20 +1,29 @@
 from tornado.concurrent import return_future
 from gcloud import storage
-from collections import defaultdict
 
-buckets = defaultdict(dict)
+buckets = {}
+
 
 @return_future
 def load(context, path, callback):
-    bucket_id  = context.config.get("CLOUD_STORAGE_BUCKET_ID")
     project_id = context.config.get("CLOUD_STORAGE_PROJECT_ID")
-    bucket = buckets[project_id].get(bucket_id, None)
-    if bucket is None:
-        client = storage.Client(project_id)
-        bucket = client.get_bucket(bucket_id)
-        buckets[project_id][bucket_id] = bucket
 
+    # get the app key from path
+    b = path.split('/').get(0, None)
+
+    # load buckets
+    if len(buckets) == 0:
+        # bucket config, dictionary with app_name => bucket_id
+        bucket_settings = context.config.get("CLOUD_STORAGE_BUCKETS")
+        # Update bucket list with configured buckets
+        for k, v in bucket_settings.items():
+            client = storage.Client(project_id)
+            bucket = client.get_bucket(v)
+            buckets[k] = bucket
+
+    bucket = buckets.get(b)
     blob = bucket.get_blob(path)
+
     if blob:
         callback(blob.download_as_string())
     else:
